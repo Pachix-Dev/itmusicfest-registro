9cc1c3f2f512ac3a81f86b11ed7d0ef50015e679
Dion Low <dionlow17@gmail.com>
2023-02-27 17:56:11 -0800

    refactor: move Step4Form out of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
-import { ADD_ONS } from "./constants";
-import {
-  formatCost,
-  formatPlanCostSummary,
-  formatPlanIdSummary,
-  getAddOnCost,
-  getTotalCost,
-} from "./components/form/utility";
+
 import { StepsSidebar } from "./components/form/StepsSidebar";
 import { Step1Form } from "./components/form/Step1Form";
 import { Step2Form } from "./components/form/Step2Form";
 import { Step3Form } from "./components/form/Step3Form";
+import { Step4Form } from "./components/form/Step4Form";
 import { Step5Form } from "./components/form/Step5Form";
 import {
   onValidateStep1,
@@ -24,81 +18,6 @@ import {
   useForm,
   useFormDispatch,
 } from "./reducers/FormContext";
-
-/**
- *
- * @param {string} addOn add_on_multiplayer, add_on_profile, add_on_storage
- * @returns {JSX.Element}
- */
-export function AddOnRow({ addOnEnum, isYearly }) {
-  const { title } = ADD_ONS[addOnEnum];
-  const addOnCost = getAddOnCost(addOnEnum, isYearly);
-  const costString = formatCost(addOnCost, isYearly, true);
-
-  return (
-    <div className="summary-row">
-      <p>{title}</p>
-      <div className="summary-cost">{costString}</div>
-    </div>
-  );
-}
-
-
-function Step4Form({ formState, setStepNo }) {
-  const planIdSummary = formatPlanIdSummary(formState);
-  const planCost = formatPlanCostSummary(formState);
-  const { add_on_multiplayer, add_on_storage, add_on_profile } = formState;
-  const totalCost = formatCost(
-    getTotalCost(formState),
-    formState.isYearly,
-    true
-  );
-
-  // if no planId or yearly selection is made: go back to step 2.
-  useEffect(() => {
-    if (planIdSummary === null) {
-      console.log("redirecting: user has not selected planId or isYearly yet ");
-      setStepNo(2);
-    }
-  }, [planIdSummary]);
-
-  return (
-    <div className="form-container">
-      <h2>Finishing up</h2>
-      <p className="mb-1">Double-check everything looks OK before confirming</p>
-      <div className="summary">
-        <div className="summary-row border-b">
-          <div>
-            <p className="text-primary text-bold-600">{planIdSummary}</p>
-            <p className="internal-link" onClick={() => setStepNo(2)}>
-              change
-            </p>
-          </div>
-          <div className="text-primary text-bold-600">{planCost}</div>
-        </div>
-        {add_on_multiplayer && (
-          <AddOnRow
-            addOnEnum="add_on_multiplayer"
-            isYearly={formState.isYearly}
-          />
-        )}
-        {add_on_storage && (
-          <AddOnRow addOnEnum="add_on_storage" isYearly={formState.isYearly} />
-        )}
-        {add_on_profile && (
-          <AddOnRow addOnEnum="add_on_profile" isYearly={formState.isYearly} />
-        )}
-      </div>
-      <div className="summary-row p-1">
-        <p className="text-bold-600">{`Total (per ${
-          formState.isYearly ? "year" : "month"
-        })`}</p>
-        <p className="text-secondary text-bold-600">{totalCost}</p>
-      </div>
-    </div>
-  );
-}
-
 
 /**
  * Given a step, will render the proper StepForm
