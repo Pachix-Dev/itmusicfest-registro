471d1f833261608c2868bfe62e59da8772010482
Pachi-Dev <pachi.claros@gmail.com>
2023-09-15 14:38:42 -0600

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,8 @@ app.use(
   cors({
     origin: (origin, callback) => {
       const ACCEPTED_ORIGINS = [
-        'http://3.133.150.190',
-        'http://3.133.150.190:1234',
-        'http://localhost:5173',
+        'http://localhost',
+        // 'http://localhost:5173',
         'http://localhost:1234'
       ]
 
